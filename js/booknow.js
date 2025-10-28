ba155a04d2c1bd6906357e87bf82fbada0c50485
akshashaikh121 <akshashaikh228@gmail.com>
2025-10-28 16:23:21 +0530

    update

--- js/booknow.js
+++ js/booknow.js
@@ -4,3 +4,18 @@ let card_value = document.getElementById(card).innerHTML
   localStorage.setItem("selectedCard", card_value); // store data
   window.location.href = "booknowpage.html"; // go to next page
 }
+function moreItem(){
+  let items = document.getElementById("more-item")
+  items.classList.remove("d-none")
+  document.getElementById("btn1").classList.add("d-none")
+    document.getElementById("btn2").classList.remove("d-none")
+}
+function lessItem(){
+    document.getElementById("btn1").classList.remove("d-none")
+  document.getElementById("btn2").classList.add("d-none")
+    let items = document.getElementById("more-item")
+  items.classList.add("d-none")
+
+
+
+}
